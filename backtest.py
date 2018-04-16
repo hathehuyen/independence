51ef0b1caa14d1d928f9c4d7c0b33c5ae1f119ee
Huyen Ha <hathehuyen@gmail.com>
2018-04-16 20:30:03 +0700

    Visualize test result

--- backtest.py
+++ backtest.py
@@ -6,6 +6,9 @@ from db import *
 from common.ohlcv import OHLCV
 from common.position import Position
 from strategies.volume_support_resistance import VolumeSupportResistance
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 # Log config
 # logger = logging.getLogger(__name__)
@@ -15,17 +18,17 @@ logging.basicConfig(level=logging.DEBUG)
 # Margin available
 margin = 1000
 # Trailing stop percent
-trailing_profit_pct = 30
+trailing_profit_pct = 60
 trailing_stop_pct = 1
 # Stop loss percent
-stop_pct = 30
+stop_pct = 60
 # Candle length milisecond
 # 5 seconds
 # candle_period = 5000
-candle_period = 60000
-min_length = 1 * 30
-max_length = 24 * 7 * 30
-diff = 3
+candle_period = 60000 * 60
+min_length = 2
+max_length = 2 * 24
+diff = 5
 # Strategy look back length
 # # 1 hours
 # strategy_length = 720
@@ -43,9 +46,9 @@ diff = 3
 # # 6 hours
 # strategy_length = 4320
 # Back test data settings
-selector = 'bitfinex.ETP-USD'
-start = time.mktime(datetime.strptime('201709010000', "%Y%m%d%H%M%S").timetuple()) * 1000
-end = time.mktime(datetime.strptime('201803302359', "%Y%m%d%H%M%S").timetuple()) * 1000
+selector = 'bitfinex.BTC-USD'
+start = time.mktime(datetime.strptime('201803150000', "%Y%m%d%H%M%S").timetuple()) * 1000
+end = time.mktime(datetime.strptime('201804152359', "%Y%m%d%H%M%S").timetuple()) * 1000
 
 
 # print(start, end)
@@ -61,16 +64,31 @@ pos = Position()
 positions = []
 last_signal = ''
 pl = 0
+price_index = []
+price_value = []
+buy_index = []
+buy_value = []
+sell_index = []
+sell_value = []
 for trade in trade_cursor:
     # Add trade to OHLCV candle
     candle.add_trade(trade['time'], trade['size'], trade['price'], trade['side'])
     # print(candle.start, candle.end, candle.open, candle.high, candle.low, candle.close, candle.volume)
     # Complete one OHCLV candle
     if candle.start != 0 and candle.end != 0 and candle.end - candle.start >= candle_period:
+        price_index.append(datetime.fromtimestamp(float(candle.start / 1000)))
+        price_value.append(candle.close)
         # Add candle to strategy
-        # print(candle.volume, candle.sell_vol, candle.buy_vol)
         strategy.add_candle(candle)
+        # print(candle.volume, candle.sell_vol, candle.buy_vol)
         # print(strategy.signal)
+        # if strategy.signal != last_signal:
+        #     if strategy.signal == 'buy':
+        #         buy_index.append(datetime.fromtimestamp(float(candle.start / 1000)))
+        #         buy_value.append(candle.close)
+        #     elif strategy.signal == 'sell':
+        #         sell_index.append(datetime.fromtimestamp(float(candle.start / 1000)))
+        #         sell_value.append(candle.close)
         # We got a new signal that difference from last signal
         if strategy.signal != last_signal:
             # logging.info(last_signal + '->' + strategy.signal + ':' + str(trade['price']))
@@ -79,10 +97,14 @@ for trade in trade_cursor:
             if strategy.signal == 'buy' and pos.status != 'ACTIVE' and trade['price'] != 0:
                 logging.info('Open long position @ ' + str(trade['price']) + " length: " + str(strategy.length))
                 pos.open('btcusd', float(trade['price']), margin / float(trade['price']))
+                buy_index.append(datetime.fromtimestamp(float(candle.start / 1000)))
+                buy_value.append(candle.close)
             # Open short position
             if strategy.signal == 'sell' and pos.status != 'ACTIVE' and trade['price'] != 0:
                 logging.info('Open short position @ ' + str(trade['price']) + " length: " + str(strategy.length))
                 pos.open('btcusd', float(trade['price']), -margin / float(trade['price']))
+                sell_index.append(datetime.fromtimestamp(float(candle.start / 1000)))
+                sell_value.append(candle.close)
             last_signal = strategy.signal
         # Calculate position P/L
         if pos.status == 'ACTIVE':
@@ -124,6 +146,14 @@ print('Wins/Loses:', wins, '/', loses)
 fee = len(positions) * margin * 0.004
 print('Profit:', profit - fee)
 print('ROI%: ', (profit - fee) / margin * 100)
+print(len(buy_value), len(sell_value), len(buy_index), len(sell_index))
+plt.plot(price_index, price_value, 'b-', label='Price')
+plt.plot(buy_index, buy_value, '^', markersize=10, color='g', label='Buy')
+plt.plot(sell_index, sell_value, 'v', markersize=10, color='r', label='Sell')
+plt.ylabel('USD')
+plt.xlabel('Date')
+plt.legend(loc=0)
+plt.show()
 # print('Max trading fee (taker):', fee)
 # for candle in candle_list:
 #     print(candle.start, candle.end, candle.open, candle.high, candle.low, candle.close, candle.volume)
